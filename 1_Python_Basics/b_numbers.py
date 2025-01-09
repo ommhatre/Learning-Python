03788d0c0119fdebee154f7b45c72689556bc70d
ommhatre <ommhatre.in@gmail.com>
2025-01-09 13:02:40 +0530

    numbers

---
+++ 1_Python_Basics/b_numbers.py
@@ -0,0 +1,15 @@
+# Arithmetic operators:
+# Addition:        3 + 2
+# Substraction:    3 - 2
+# Multiplication:  3 * 2
+# Division:        3 / 2
+# Floor Division:  3 // 2 
+# Exponent:        3 ** 2
+# Modulus:         3 % 2
+
+num1 = 5
+num2 = 10
+
+# Other operations
+print(num1 == num2)
+print(num1 < num2)
