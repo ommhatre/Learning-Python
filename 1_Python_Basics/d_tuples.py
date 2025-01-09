190968d9f1f3554251228e9b66ed86317e29e0b8
ommhatre <ommhatre.in@gmail.com>
2025-01-09 13:45:47 +0530

    tuples

---
+++ 1_Python_Basics/d_tuples.py
@@ -0,0 +1,10 @@
+"""
+Tuples:
+difference between tuples and list is that, tuples are immutable (cannot be modified)
+tuples are defined in round brackets ()
+"""
+
+tuple1 = ("math", "biology", "computer")
+print(tuple1)
+
+# we cannot modify tuples, but other than that we can do everything else, like search through it, access certain elements, etc
